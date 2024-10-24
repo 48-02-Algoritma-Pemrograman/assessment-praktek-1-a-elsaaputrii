2217a9d7984cd9b01b90bff472bd24c38a4b108c
elsaaputrii <elsaputri1113@gmail.com>
2024-10-24 12:44:34 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,33 @@
 public class Nomor2{
     public static void main(String[] args){
+        String namaDrive, Beliau, Mereka, Kita;
+        int lamaTrip;
+        int totalOmzet;
+        int totaltrip = 13000;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("masukan nama drive");
+        namaDrive = scanner.next();
+        System.out.print("masukan lama trip");
+        lamaTrip = scanner.nextInt();
+        totaltrip = scanner.nextInt();
+
+
+        if (namaDrive.equals("Beliau")) {
+            totaltrip =13000;
+        }
+
+        else if (namaDrive.equals("Mereka")) {
+            totaltrip = 13000;
+        }
+
+        else if (namaDrive.equals("Kita")) {
+            totaltrip = 30000;
+        }
+
+        totalOmzet = lamaTrip *totaltrip;
+        totaltrip = lamaTrip *13000;
 
     }
 }
