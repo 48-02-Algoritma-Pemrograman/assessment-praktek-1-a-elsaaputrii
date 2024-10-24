f087fa0b0442ded817e82334faee24d41eb16966
elsaaputrii <elsaputri1113@gmail.com>
2024-10-24 12:15:57 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,21 @@
 public class Nomor1{
     public static void main(String[] args){
+        int N;
+        int honorLembur= 10000;
 
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Masukkan jam lembur (dalam jam): ");
+        N = input.nextInt();
+
+        if (N <= 16) {
+            System.out.println("mendapat honor lembur:" + (N *honorLembur));
+        } else if (N >= 16) {
+            System.out.println("mendapat honor lembur:" + (N *honorLembur));
+        } else {
+            System.out.println(" mendapat honor:" + (N *honorLembur));
+        }
+
+        honorLembur = N * 10000;
     }
 }
